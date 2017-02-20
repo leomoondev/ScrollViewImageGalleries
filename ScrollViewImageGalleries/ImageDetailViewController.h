966465b222d1a4427cd5e123238a788eda58a73c
Leo Moon <leomoondev@gmail.com>
2017-02-20 15:30:04 -0800

    Added the storyboard and files for image detail view controller

---
+++ ScrollViewImageGalleries/ImageDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  ImageDetailViewController.h
+//  ScrollViewImageGalleries
+//
+//  Created by Hyung Jip Moon on 2017-02-20.
+//  Copyright © 2017 leomoon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ImageDetailViewController : UIView
+
+@end
---
+++ ScrollViewImageGalleries/ImageDetailViewController.m
@@ -0,0 +1,21 @@
+//
+//  ImageDetailViewController.m
+//  ScrollViewImageGalleries
+//
+//  Created by Hyung Jip Moon on 2017-02-20.
+//  Copyright © 2017 leomoon. All rights reserved.
+//
+
+#import "ImageDetailViewController.h"
+
+@implementation ImageDetailViewController
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
